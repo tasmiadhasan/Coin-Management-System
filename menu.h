4bd45f68b1260e8986031534b317cf4ef2eded8d
Tasmiad Hasan <tasmiadhasan@gmail.com>
2025-12-06 22:51:13 +0600

    Initially done

---
+++ menu.h
@@ -0,0 +1,378 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+void menu()
+{
+  system("CLS");
+  int choice=0;
+   splash();
+  puts("\n\n\n                                             Login Successful\n");
+  puts("\n                                            Welcome to The management System\n");
+    while(choice !=5)
+    {
+
+        puts("\n                                           Choose Country\n");
+        puts("                                             1.Bangladesh\n");
+        puts("                                             2.India\n");
+        puts("                                             3.Pakistan\n");
+        puts("                                             4.Turkey\n");
+        puts("                                             5.Exit\n");
+        printf("\n                                         Enter Your Choice:");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+        case 1:
+
+            {
+               system("CLS");
+               splash();
+                puts("\n                                 Bangladeshi all coin list\n");
+            int sub1=0;
+            while(sub1!=4)
+            {
+                puts("\n                                    Choose one...........\n");
+                puts("\n                                    1.One Taka");
+                puts("\n                                    2.Two Taka");
+                puts("\n                                    3.Five Taka");
+                puts("\n                                    4.Main Menu");
+                printf("\n                              Enter which coin you want to see:");
+                scanf("%d",&sub1);
+                if(sub1==1)
+                    {
+                        system("CLS");
+                        splash();
+                        int bd;
+                    puts("\n                                    1.For Coin Image");
+                    puts("\n                                    2.Description");
+                    printf("\n                                  Your choice is:");
+                    scanf("%d",&bd);
+                    if(bd==1)
+                { system("CLS");
+                splash();
+                system("start images\\one.jpg");}
+
+                    else if(bd==2)
+                        { system("CLS");
+                        splash();
+                            puts("\n                               Description");
+                        puts("\n                                  Made with silver.");
+                        puts("\n	                             The logo of father of the nation.");
+                        }
+                }
+                else if(sub1==2)
+                    { system("CLS");
+                    splash();
+                    int bd;
+                    puts("\n                                      1.For Coin Image");
+                    puts("\n                                      2.Description");
+                    printf("\n                                    Your choice:");
+                    scanf("%d",&bd);
+                    if(bd==1)
+                { system("CLS");
+                splash();
+                        system("start images\\two.jpg");}
+
+                    else if(bd==2)
+
+                      { system("CLS");
+                      splash();
+                        puts("\n                                       Description\n");
+                      puts("\n                                        Made with silver.");
+                      puts("\n                                   The logo of father of the nation.");
+                      }
+
+                }
+                else if(sub1==3)
+                    { system("CLS");
+                    splash();
+                    int bd;
+                    puts("\n                                       1.For Coin Image");
+                    puts("\n                                       2.Description");
+                    printf("\n                                     Your choice:");
+                    scanf("%d",&bd);
+                    if(bd==1)
+                { system("CLS");
+                splash();
+                    system("start images\\five.jpg");}
+
+                    else if(bd==2)
+                        { system("CLS");
+                        splash();
+                        puts("\n                                     Description\n");
+                         puts("\n                                    Made with silver.");
+                         puts("\n                                   The logo of Jamuna Bridge.");
+                         }
+
+                }
+                else if(sub1==4)
+                {
+                    system("CLS");
+                    splash();
+                    menu();
+                }
+
+            }
+            }
+            break;
+        case 2:
+            { system("CLS");
+            splash();
+             puts("\n                                                Indian all coin list\n");
+            int sub2=0;
+            while(sub2!=4)
+            {
+                puts("\n                                                Choose one...........\n");
+                puts("\n                                               1.One Rupee");
+                puts("\n                                               2.Two Rupee");
+                puts("\n                                               3.Five Rupee");
+                puts("\n                                               4.Main Menu");
+                printf("\n                                      Enter which coin u want to see >>");
+                scanf("%d",&sub2);
+                if(sub2==1)
+                    { system("CLS");
+                    splash();
+                        int in;
+                    puts("\n                                          1.For Coin Image");
+                    puts("\n                                          2.Description");
+                    printf("\n                                        Your choice:");
+                    scanf("%d",&in);
+                    if(in==1)
+                       { system("CLS");
+                       splash();
+                           system("start images\\oneru.jpg");}
+
+                    else if(in==2)
+                        { system("CLS");
+                        splash();
+                        puts("\n                                       Description\n");
+                        puts("\n                                       Made with silver.");
+                        puts("\n                                       The Logo of Paddy.");
+                        }
+                }
+                else if(sub2==2)
+                    { system("CLS");
+                    splash();
+                    int in;
+                    puts("\n                                        1.For Coin Image");
+                    puts("\n                                        2.Description");
+                    printf("\n                                      Your choice:");
+                    scanf("%d",&in);
+                    if(in==1)
+                { system("CLS");
+                splash();
+                    system("start images\\tworu.jpg");}
+
+                    else if(in==2)
+                        { system("CLS");
+                        splash();
+                        puts("\n                                      Description\n");
+                        puts("\n                                      Made with silver.");
+                        puts("\n                                 The Logo of government of India.");}
+                }
+                else if(sub2==3)
+                    { system("CLS");
+                    splash();
+                    int in;
+                    puts("\n                                        1.For Coin Image");
+                    puts("\n                                        2.Description");
+                    printf("\n                                      Your choice:");
+                    scanf("%d",&in);
+                    if(in==1)
+                { system("CLS");
+                splash();
+                    system("start images\\5ru.jpg");}
+                    else if(in==2)
+                        { system("CLS");
+                        splash();
+                        puts("\n                                     Description\n");
+                        puts("\n                                     Made with silver.");
+                        puts("\n                                Coin Color Gold. The Logo of government of India.");}
+                }
+                else if(sub2==4)
+                {
+                    system("CLS");
+                    splash();
+                    menu();
+                }
+            }
+            }
+            break;
+            case 3:
+            { system("CLS");
+            splash();
+             puts("\n                                          Pakistani all coin list\n");
+            int sub3=0;
+            while(sub3!=4)
+            {
+                puts("\n                                         Choose one...........\n");
+                puts("\n                                           1.One Rupee");
+                puts("\n                                           2.Two Rupee");
+                puts("\n                                           3.Five Rupee");
+                puts("\n                                           4.Main Menu");
+                printf("\n                                 Enter which coin u want to see:");
+                scanf("%d",&sub3);
+                if(sub3==1)
+                    { system("CLS");
+                    splash();
+                    int pk;
+                    puts("\n                                       1.For Coin Image");
+                    puts("\n                                       2.Description");
+                    printf("\n                                     Your choice:");
+                    scanf("%d",&pk);
+                    if(pk==1)
+                       { system("CLS");
+                           splash();
+                           system("start images\\p1.jpg");}
+
+                    else if(pk==2)
+                        { system("CLS");
+                        splash();
+                        puts("\n                                     Description\n");
+                        puts("\n                                     Made with silver.");
+                        puts("\n                                   The Logo of moon and star.");}
+                }
+                else if(sub3==2)
+                    { system("CLS");
+                    splash();
+                    int pk;
+                    puts("\n                                         1.For Coin Image");
+                    puts("\n                                         2.Description");
+                    printf("\n                                       Your choice:");
+                    scanf("%d",&pk);
+                    if(pk==1)
+                { system("CLS");
+                splash();
+                    system("start images\\p2.jpg");}
+
+                    else if(pk==2)
+                        { system("CLS");
+                        splash();
+                        puts("\n                                     Description\n");
+                        puts("\n                                   Made with silver.");
+                        puts("\n                                  The Logo of Mosque.");}
+                }
+                else if(sub3==3)
+                    { system("CLS");
+                    splash();
+                    int pk;
+                    puts("\n                                        1.For Coin Image");
+                    puts("\n                                        2.Description");
+                    printf("\n                                      Your choice:");
+                    scanf("%d",&pk);
+                    if(pk==1)
+                { system("CLS");
+                splash();
+                    system("start images\\p5.jpg");}
+                    else if(pk==2)
+                        { system("CLS");
+                        splash();
+                        puts("\n                                     Description\n");
+                        puts("\n                                     Made with silver.");
+                        puts("\n                                  The Logo of moon and star.");}
+                }
+                else if(sub3==4)
+                {
+                    system("CLS");
+                    splash();
+                    menu();
+                }
+            }
+            }
+            break;
+            case 4:
+            {
+            system("CLS");
+            splash();
+             puts("\n                                         Turkish all coin list\n");
+            int sub4=0;
+            while(sub4!=4)
+            {
+
+               puts("\n                                           Choose one...........\n");
+                puts("\n                                           1.One Lira");
+                puts("\n                                           2.Two Lira");
+                puts("\n                                           3.Five Lira");
+                puts("\n                                           4.Main Menu");
+                printf("\n                                  Enter which coin u want to see:");
+                scanf("%d",&sub4);
+                if(sub4==1)
+                    { system("CLS");
+                    splash();
+                    int tk;
+                    puts("\n                                     1.For Coin Image");
+                    puts("\n                                     2.Description");
+                    printf("\n                                    Your choice:");
+                    scanf("%d",&tk);
+                    if(tk==1)
+                       { system("CLS");
+                       splash();
+                           system("start images\\1lira.jpg");}
+
+                    else if(tk==2)
+                        { system("CLS");
+                        splash();
+                        puts("\n                                 Description\n");
+                        puts("\n                                Made with silver.");
+                        puts("\n                              The Logo of Hagia Sophia.");
+
+                        }
+                }
+                else if(sub4==2)
+                    { system("CLS");
+                    splash();
+                    int tk;
+                    puts("\n                                     1.For Coin Image");
+                    puts("\n                                     2.Description");
+                    printf("\n                                   Your choice:");
+                    scanf("%d",&tk);
+                    if(tk==1)
+                { system("CLS");
+                splash();
+                    system("start images\\2lira.jpg");}
+
+                    else if(tk==2)
+                        { system("CLS");
+                        splash();
+                        puts("\n                                 Description\n");
+                        puts("\n                                Made with silver.");
+                        puts("\n                              The Logo of a farmer.");}
+                }
+                else if(sub4==3)
+                    { system("CLS");
+                    splash();
+                    int tk;
+                    puts("\n                                      1.For Coin Image");
+                    puts("\n                                      2.Description");
+                    printf("\n                                    Your choice:");
+                    scanf("%d",&tk);
+                    if(tk==1)
+                { system("CLS");
+                splash();
+                    system("start images\\5lira.jpg");}
+                    else if(tk==2)
+                        { system("CLS");
+                        splash();
+                        puts("\n                               Description\n");
+                        puts("\n                              Made with silver.");
+                        puts("\n                            The Logo of a horse man.");}
+                }
+                else if(sub4==4)
+                {
+                    system("CLS");
+                    splash();
+                    menu();
+                }
+            }
+            }
+            break;
+            case 5:
+                { system("CLS");
+                splash();
+                    printf("\n                                         You Exited the program.\n\n");
+                    return;
+                }
+
+        }
+}
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include<string.h>
+#include <stdlib.h>
+#include <conio.h>
+#include "splash.h"
+#include "menu.h"
+
+/* Forward declarations */
+void login();
+void regis();
+
+struct login{
+char fname[100];
+char lname[100];
+char username[100];
+char password[100];
+};
+
+int main()
+{
+    int cho;
+    puts("                                             Welcome to Coin Management System\n");
+    puts("\n                                              Please Follow the security Steps\n");
+    printf("                                           1.Sign Up\n");
+    printf("                                           2.Sign In\n\n");
+
+    puts("                                             Press the number & Enter\n");
+    printf("                                           >>>>");
+
+    scanf("%d",&cho);
+    if(cho==1)
+    {
+        system("CLS");
+        regis();
+    }
+    else if(cho==2)
+        {
+        system("CLS");
+        login();}
+}
+
+
+
+void login()
+{
+    char username[200],password[20];
+    FILE*log;
+    log=fopen("login.txt","r");
+    struct login l;
+    puts("                                                  Sign In\n");
+    printf("                                             Enter Username:");
+    scanf("%s",&username);
+    printf("                                             Enter Password:");
+    scanf("%s",&password);
+    while(fread(&l,sizeof(l),1,log))
+    {
+        if(strcmp(username,l.username)==0 && strcmp(password,l.password)==0)
+        {
+            printf("                                      Login Successful\n\n");
+            menu();
+        }
+        else{
+            printf("                                        Incorrect Password...type again");
+            system("CLS");
+
+            login();
+        }
+    }
+    fclose(log);
+}
+
+
+void regis()
+{
+    FILE*log;
+    log=fopen("login.txt","w");
+    struct login l;
+    puts("                                     Coin Management Security system Registration\n\n");
+    printf("                                               Enter first name:");
+    scanf("%s",l.fname);
+    printf("                                               Enter last name:");
+    scanf("%s",l.lname);
+    printf("                                               Enter Username:");
+    scanf("%s",l.username);
+    printf("                                               Enter Password:");
+    scanf("%s",l.password);
+    fwrite(&l,sizeof(l),1,log);
+    fclose(log);
+    printf("\n                                   Please Remember your user name & password!!!\n\n");
+    printf("                                           PRESS ANY KEY TO CONTINUE");
+    getch();
+    system("CLS");
+    login();
+}
